85280d47febee3eb9ce1705281ab34d356ea63d2
CaMeLCa5e <jm273606@gmail.com>
2014-10-13 14:07:08 -0400

    chapter updates

---
+++ thehardway40.py
@@ -0,0 +1,34 @@
+#40
+
+mystuff = {"apple": "I am apples!"}
+print mystuff ['apple']
+
+def apple():
+	print "I am apples!"
+	
+import mystuff
+mystuff.apple()
+
+def apple(): 
+	print "I am apples"
+	
+tangerine = "Living reflection of a dream"
+
+import mystuff
+
+mystuff.apple()
+print mystuff.tangerine
+
+mystuff["apple"]
+mystuff.apple()
+mystuff.tangerine
+
+class MyStuff(object):
+	
+	def __init__ (self):
+		self.tangerine = "And a thousand years in between"
+		
+	def apple(self):
+		print "I am classy apples!"
+		
+		
